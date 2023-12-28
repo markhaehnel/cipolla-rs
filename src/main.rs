8ae910123b0fcd19192fa6fd43413c1ec587d84e
Mark Hähnel <1516205+markhaehnel@users.noreply.github.com>
2023-12-28 20:44:44 +0100

    chore: remove axum and tower (#7)

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,9 @@ use std::{net::SocketAddr, sync::LazyLock};
 
 use anyhow::Result;
 use arti_client::TorClient;
-use axum::{
-    body::Body,
-    http::{Method, Request},
-    Router,
-};
+use hyper::{server::conn::http1, service::service_fn};
+use hyper_util::rt::TokioIo;
 use tor_rtcompat::PreferredRuntime;
-use tower::{make::Shared, ServiceExt};
-use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 use cipolla::{cli, proxy::proxy, tor::build_tor_client};
@@ -27,7 +22,7 @@ async fn main() -> Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "cipolla=debug,axum::rejection=trace".into()),
+                .unwrap_or_else(|_| "cipolla=debug".into()),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
@@ -38,29 +33,22 @@ async fn main() -> Result<()> {
     let port_from = ARGS.port;
     let port_to = port_from + num_ports;
 
-    let router = Router::new().layer(TraceLayer::new_for_http());
-
-    let service = tower::service_fn(move |req: Request<Body>| {
-        let router_svc = router.clone();
-        async move {
-            if req.method() == Method::CONNECT {
-                proxy(req, &TOR_CLIENT).await
-            } else {
-                router_svc.oneshot(req).await.map_err(|err| match err {})
-            }
-        }
-    });
-
     for port in port_from..port_to {
-        let service_clone = service.clone();
         let job = tokio::spawn(async move {
             let addr: SocketAddr = format!("[::]:{port}").parse().expect("Invalid port");
 
-            axum::Server::bind(&addr)
-                .http1_preserve_header_case(true)
-                .http1_title_case_headers(true)
-                .serve(Shared::new(service_clone))
-                .await
+            let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+            loop {
+                let (stream, _) = listener.accept().await.unwrap();
+                let io = TokioIo::new(stream);
+
+                let _ = http1::Builder::new()
+                    .preserve_header_case(true)
+                    .title_case_headers(true)
+                    .serve_connection(io, service_fn(|req| proxy(req, &TOR_CLIENT)))
+                    .with_upgrades()
+                    .await;
+            }
         });
 
         handles.push(job);
@@ -69,7 +57,7 @@ async fn main() -> Result<()> {
     tracing::info!("listening on ports {} to {}", port_from, port_to - 1);
 
     for handle in handles {
-        let _ = handle.await?;
+        handle.await?;
     }
 
     Ok(())
--- src/proxy.rs
+++ src/proxy.rs
@@ -1,16 +1,15 @@
 use arti_client::TorClient;
-use axum::{
-    body,
-    response::{IntoResponse, Response},
-};
-use hyper::{upgrade::Upgraded, Body, Request, StatusCode};
+use bytes::Bytes;
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::{upgrade::Upgraded, Request, Response, StatusCode};
+use hyper_util::rt::TokioIo;
 use tor_rtcompat::PreferredRuntime;
 
 #[tracing::instrument(skip(tor_client))]
 pub async fn proxy(
-    req: Request<Body>,
+    req: Request<hyper::body::Incoming>,
     tor_client: &'static TorClient<PreferredRuntime>,
-) -> Result<Response, hyper::Error> {
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
     if let Some(host_addr) = req.uri().authority().map(ToString::to_string) {
         tracing::debug!("{}", host_addr);
         tokio::task::spawn(async move {
@@ -24,25 +23,25 @@ pub async fn proxy(
             }
         });
 
-        Ok(Response::new(body::boxed(body::Empty::new())))
+        Ok(Response::new(empty()))
     } else {
-        tracing::error!("CONNECT host is not socket addr: {:?}", req.uri());
-        Ok((
-            StatusCode::BAD_REQUEST,
-            "CONNECT must be to a socket address",
-        )
-            .into_response())
+        tracing::warn!("CONNECT host is not socket addr: {:?}", req.uri());
+        let mut resp = Response::new(full("CONNECT must be to a socket address"));
+        *resp.status_mut() = StatusCode::BAD_REQUEST;
+
+        Ok(resp)
     }
 }
 
 async fn tunnel(
-    mut upgraded: Upgraded,
+    upgraded: Upgraded,
     addr: String,
     tor_client: &'static TorClient<PreferredRuntime>,
 ) -> std::io::Result<()> {
     let tor_client = tor_client.isolated_client();
 
     let mut server = tor_client.connect(addr).await.unwrap();
+    let mut upgraded = TokioIo::new(upgraded);
 
     let (from_client, from_server) =
         tokio::io::copy_bidirectional(&mut upgraded, &mut server).await?;
@@ -54,4 +53,16 @@ async fn tunnel(
     );
 
     Ok(())
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
